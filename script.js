e90bbb63298dea2568a28aaf1a7b5076a20b624f
codingsake <thinthazin.chris@gmail.com>
2022-01-18 09:28:47 -0800

    heart btn work

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ function myFunction(el) {
   popup.classList.toggle("show");
 }
 
+
+document.querySelectorAll('.heart-btn').forEach(item => {
+  item.addEventListener('click', event => {
+
+    var div = item.querySelector('.content');
+    var btn = div.querySelector('.likeBtn');
+    var span = div.querySelector('.display');
+
+    if (btn.src.match("images/heart-regular.svg")) {
+      btn.src = "images/heart-solid.svg";
+      var count = 1;
+      span.innerHTML = count;
+    } else {
+      btn.src = "images/heart-regular.svg";
+      var count = 0;
+      span.innerHTML = count;
+    }
+
+  })
+});
+
 // Heart funciton
 
 //initialize the count to 0.
@@ -14,19 +35,19 @@ var disp = document.getElementById("display");
 function changeHeart() {
   // func for heart icon
   var heartId = document.getElementById("btn");
-  
+
   // initializing var heartId to 'heart' id in html file
   if (heartId.src.match("images/heart-regular.svg")) {
-      // if src of heart img is regular then change to solid
-      heartId.src = "images/heart-solid.svg";
-      //set the count to one and print when the heart change to solid
-      var count = 1;
-      disp.innerHTML = count;
+    // if src of heart img is regular then change to solid
+    heartId.src = "images/heart-solid.svg";
+    //set the count to one and print when the heart change to solid
+    var count = 1;
+    disp.innerHTML = count;
   } else {
-      // change back to regular
-      heartId.src = "images/heart-regular.svg";
-      //set the count to 0 and print when the heart change to regular
-      var count = 0;
-      disp.innerHTML = count;
-    }
+    // change back to regular
+    heartId.src = "images/heart-regular.svg";
+    //set the count to 0 and print when the heart change to regular
+    var count = 0;
+    disp.innerHTML = count;
+  }
 }
